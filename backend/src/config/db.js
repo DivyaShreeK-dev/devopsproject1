bd59ebae51c7af03088fbdcabf884b7ceb3d3de8
Divya <divyashreedivi28@gmail.com>
2026-04-04 13:19:19 +0530

    Updated

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -5,13 +5,12 @@ dotenv.config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb+srv://divyashree:Divya@3058@cluster0.hiqlka2.mongodb.net/mydb");
-    console.log("MongoDB Atlas connected");
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("MongoDB connected");
   } catch (error) {
     console.error("DB connection error:", error.message);
     process.exit(1);
   }
 };
-
 
 module.exports = connectDB;
